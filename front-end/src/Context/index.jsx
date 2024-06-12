dc5b629d2d72dc9d5db8284bc97ad7c021930909
alejandramantillac <alejamantillac@gmail.com>
2024-06-12 16:11:59 -0500

    feat: product rendering

--- front-end/src/Context/index.jsx
+++ front-end/src/Context/index.jsx
@@ -1,10 +1,71 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 
 export const GlobalContext = createContext();
 
 export const GlobalProvider = ({ children }) => {
+
+  // Products
+  const [items, setItems] = useState(null)
+
+  // Products - Filter
+  const [filteredItems, setFilteredItems] = useState(null)
+
+  // Product Detail - Open/Close
+  const [openProductDetail, setOpenProductDetail] = useState(false)
+
+  // Product Detail - Show Product
+  const [productToShow, setProductToShow] = useState(null)
+
+  // Shopping Cart - Quantity
+  const [count, setCount] = useState([])
+
+  // Shopping Cart - Add products to cart
+  const [cartProducts, setCartProducts] = useState([])
+
+  // Get products by title
+  const [searchByTitle, setSearchByTitle] = useState(null)
+
+  // Get products by category
+  const [searchByCategory, setSearchByCategory] = useState(null)
+
+  useEffect(() => { 
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+  }, [])
+
+  const filterBy = (type,items,search) => {
+    if(type === "TITLE") return items?.filter(item => item.title.toLowerCase().includes(search.toLowerCase()));
+    if(type === "CATEGORY") return items?.filter(item => item.category.toLowerCase().includes(search.toLowerCase()));
+  }
+
+  useEffect(() => {
+    let tempItems = items;
+    if (searchByTitle) tempItems = filterBy("TITLE", tempItems, searchByTitle);
+    if(searchByCategory) tempItems = filterBy("CATEGORY", tempItems, searchByCategory);
+
+    setFilteredItems(tempItems)
+  }, [items, searchByTitle, searchByCategory]);
+  
   return (
-    <GlobalContext.Provider value={{}}>
+    <GlobalContext.Provider value={{
+      items,
+      setItems,
+      count,
+      setCount,
+      filteredItems,
+      setFilteredItems,
+      openProductDetail,
+      setOpenProductDetail,
+      productToShow,
+      setProductToShow,
+      cartProducts,
+      setCartProducts,
+      searchByTitle,
+      setSearchByTitle,
+      searchByCategory,
+      setSearchByCategory
+    }}>
         {children}
     </GlobalContext.Provider>
   );
--- front-end/src/Pages/Home/index.jsx
+++ front-end/src/Pages/Home/index.jsx
@@ -1,9 +1,30 @@
-const Home = () => {
+import { useContext } from 'react'
+import Card from '../../Components/Card'
+import Layout from '../../Components/Layout'
+import { GlobalContext } from '../../Context'
+
+function Home() {
+  const context = useContext(GlobalContext)
+
+  const renderView = () => {
+    const items = context.items || [];
+    return items.length > 0 
+      ? items.map((item, index) => <Card key={index} data={item} />)
+      : <p>Loading...</p>;
+  }
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
-};
+    <Layout>
+      <input
+        className ='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
+        type = 'text'
+        placeholder = 'Search a product...'>
+      </input>
+      <div className="grid grid-cols-4 gap-4">
+        {renderView()}
+      </div>
+    </Layout>
+  )
+}
 
 export default Home;
