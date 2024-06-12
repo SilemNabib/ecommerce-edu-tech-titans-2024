f1a41e5022c4bd4ae56e6a4a61bfabc2e545efcc
Andrewdpg <andres.parra@u.icesi.edu.co>
2024-06-12 16:50:08 -0500

    feat: user repository and ApplicationConfig
    Determine how the credentials will be validated

---
+++ back-end/src/main/java/com/sunflowers/ecommerce/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.sunflowers.ecommerce.repository;
+
+import com.sunflowers.ecommerce.entity.User.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
+
