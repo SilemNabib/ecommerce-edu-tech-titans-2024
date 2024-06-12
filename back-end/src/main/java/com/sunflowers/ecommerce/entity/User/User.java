f8103467d35c6e1b337db08db69306e9f317fe6d
Andrewdpg <andres.parra@u.icesi.edu.co>
2024-06-12 16:47:02 -0500

    feat: user-entity

---
+++ back-end/src/main/java/com/sunflowers/ecommerce/entity/User/User.java
@@ -0,0 +1,60 @@
+package com.sunflowers.ecommerce.entity.User;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.sql.Date;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+
+    private String password;
+    private String phone;
+    private Date registrationDate;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
