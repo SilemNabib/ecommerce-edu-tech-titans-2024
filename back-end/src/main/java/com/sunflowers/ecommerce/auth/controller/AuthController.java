cfa56214d525ee1aeab8c7b93b162d9fdbff684d
Andrewdpg <andres.parra@u.icesi.edu.co>
2024-06-15 11:35:49 -0500

    feat: abstract exception handling

--- back-end/src/main/java/com/sunflowers/ecommerce/auth/controller/AuthController.java
+++ back-end/src/main/java/com/sunflowers/ecommerce/auth/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.sunflowers.ecommerce.auth.request.VerificationRequest;
 import com.sunflowers.ecommerce.auth.response.AuthResponse;
 import com.sunflowers.ecommerce.auth.request.LoginRequest;
 import com.sunflowers.ecommerce.auth.request.RegisterRequest;
+import com.sunflowers.ecommerce.auth.response.ErrorResponse;
 import com.sunflowers.ecommerce.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.exception.ConstraintViolationException;
@@ -13,10 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.*;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Controller for handling authentication requests.
@@ -39,16 +37,7 @@ public class AuthController {
     @Transactional(readOnly = true)
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
-        try {
-            return ResponseEntity.ok(authService.login(loginRequest));
-        } catch (ConstraintViolationException | IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
-        }catch (AuthenticationServiceException | DisabledException | LockedException | AccountExpiredException |
-                CredentialsExpiredException e) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return ResponseEntity.ok(authService.login(loginRequest));
     }
 
     /**
@@ -61,13 +50,7 @@ public class AuthController {
      */
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest registerRequest) {
-        try {
-            return ResponseEntity.ok(authService.register(registerRequest));
-        } catch (ConstraintViolationException | IllegalArgumentException e) {
-            return ResponseEntity.badRequest().build();
-        }  catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return ResponseEntity.ok(authService.register(registerRequest));
     }
 
     /**
@@ -80,13 +63,7 @@ public class AuthController {
      */
     @PostMapping("/verify")
     public ResponseEntity<AuthResponse> verify(@RequestBody VerificationRequest verifyRequest) {
-        try {
-            return ResponseEntity.ok(authService.verify(verifyRequest));
-        } catch (ConstraintViolationException e) {
-            return ResponseEntity.badRequest().build();
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return ResponseEntity.ok(authService.verify(verifyRequest));
     }
 
     /**
@@ -100,12 +77,50 @@ public class AuthController {
     @Transactional(rollbackFor = Exception.class)
     @PostMapping("/complete")
     public ResponseEntity<AuthResponse> completeRegistration(@RequestBody CompleteRegistrationRequest request) {
-        try {
-            return ResponseEntity.ok(authService.completeRegistration(request));
-        } catch (ConstraintViolationException e) {
-            return ResponseEntity.badRequest().build();
-        } catch (Exception e){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        return ResponseEntity.ok(authService.completeRegistration(request));
+    }
+
+    /**
+     * Handles constraint violation exceptions.
+     * This method will return a bad request response with an error message.
+     *
+     * @param e the constraint violation exception
+     * @return the error response containing the error message
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(ConstraintViolationException e) {
+        return ResponseEntity.badRequest().body(new ErrorResponse("Constraint Violation", e.getMessage()));
+    }
+
+    /**
+     * Handles authentication exceptions.
+     * This method will return an unauthorized response with an error message.
+     *
+     * @param e the authentication exception
+     * @return the error response containing the error message
+     */
+    @ExceptionHandler({
+            AuthenticationServiceException.class,
+            DisabledException.class,
+            LockedException.class,
+            AccountExpiredException.class,
+            CredentialsExpiredException.class
+    })
+    public ResponseEntity<ErrorResponse> handleAuthenticationExceptions(Exception e) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new ErrorResponse("Authentication Error", e.getMessage()));
+    }
+
+    /**
+     * Handles all other exceptions.
+     * This method will return an internal server error response with an error message.
+     *
+     * @param e the exception
+     * @return the error response containing the error message
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponse("Internal Server Error", e.getMessage()));
     }
 }
