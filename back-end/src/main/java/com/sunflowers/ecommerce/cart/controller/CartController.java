7273e1a2fd24c6e594e2dc56e64f458a3f1a6650
SilemNabib <silemnabib@gmail.com>
2024-06-20 13:32:14 -0500

    build: add CartController

---
+++ back-end/src/main/java/com/sunflowers/ecommerce/cart/controller/CartController.java
@@ -0,0 +1,29 @@
+package com.sunflowers.ecommerce.cart.controller;
+
+import com.sunflowers.ecommerce.cart.entity.CartItem;
+import com.sunflowers.ecommerce.cart.request.AddProductToCartRequest;
+import com.sunflowers.ecommerce.cart.service.CartService;
+import com.sunflowers.ecommerce.response.GeneralResponse;
+import jakarta.servlet.http.HttpServletRequest;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/cart")
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/add")
+    public ResponseEntity<GeneralResponse<CartItem>> addItemToCart(@NonNull HttpServletRequest servletRequest, @RequestBody AddProductToCartRequest request) {
+        return cartService.addProductToCart(servletRequest, request);
+    }
+}
