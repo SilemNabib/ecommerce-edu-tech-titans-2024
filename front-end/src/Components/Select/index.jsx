53f5720bcb4b38485d314b74e9737bfde8b35cc5
alejandramantillac <alejamantillac@gmail.com>
2024-06-20 00:14:07 -0500

    feat: improve inventory page ui and functionality

--- front-end/src/Components/Select/index.jsx
+++ front-end/src/Components/Select/index.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-const Select = ({ options, placeholder }) => {
+const Select = ({ options, placeholder, className }) => {
   const [selectedOption, setSelectedOption] = useState('');
 
   const handleChange = (event) => {
@@ -8,7 +8,11 @@ const Select = ({ options, placeholder }) => {
   };
 
   return (
-    <select value={selectedOption} onChange={handleChange}>
+    <select
+      value={selectedOption}
+      onChange={handleChange}
+      className={`block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-500 ${className}`}
+    >
       <option value="">{placeholder}</option>
       {options.map((option, index) => (
         <option key={index} value={option}>
--- front-end/src/Pages/AdminDashboard/ProductManagement/AddProductInventory/index.jsx
+++ front-end/src/Pages/AdminDashboard/ProductManagement/AddProductInventory/index.jsx
@@ -26,19 +26,24 @@ const AddProductInventory = () => {
   return (
     <div className="container mx-auto p-4">
       <ProgressBar steps={registerSteps} currentStep={1} title="Product Creation Progress" className="mb-4" />
-      <div className="w-full border border-gray-300 p-4 rounded-md mt-4">
-        <h2 className="mb-2">Inventory</h2>
+      <div className="w-full bg-white shadow-md rounded-lg p-6 mt-4">
+        <h2 className="text-xl font-semibold mb-4">Inventory</h2>
         {specifications.map((_, index) => (
-          <ProductSpecification
-            key={index}
-            index={index + 1}
-            sizes={sizes}
-            colors={colors}
-            onRemove={() => handleRemove(index)}
-          />
+          <div key={index} className="mb-4">
+            <ProductSpecification
+              key={index}
+              index={index + 1}
+              sizes={sizes}
+              colors={colors}
+              onRemove={() => handleRemove(index)}
+            />
+          </div>
         ))}
-        <button onClick={handleAdd} className="mt-4">
-          New Inventory
+        <button
+          onClick={handleAdd}
+          className="mt-4 px-4 py-2 bg-black text-white rounded-md hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+        >
+          Add Inventory
         </button>
       </div>
     </div>
