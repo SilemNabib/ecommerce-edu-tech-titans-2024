a897bf85b1872818af90fb14c854b02418ffa703
alejandramantillac <alejamantillac@gmail.com>
2024-06-22 10:52:50 -0500

    refactor: update profile component to fetch user profile data dynamically

--- front-end/src/Pages/InformationProfile/index.jsx
+++ front-end/src/Pages/InformationProfile/index.jsx
@@ -1,14 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { UserInfo } from '../../config/UserInfo';
+import { useEffect, useState } from 'react';
 import ProfileNavigation from '../../Components/ProfileNavigation';
+import { useAuth } from '../../Context/AuthContext';
+import { ApiConfig } from '../../config/ApiConfig';
 
 const Profile = () => {
+  const [userInfo, setUserInfo] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    registrationDate: '',
+  });
+  const auth = useAuth();
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await auth.authFetch(ApiConfig.profile);
+        setUserInfo(response.data);
+      } catch (error) {
+        console.error("Failed to fetch profile", error);
+      }
+    };
+
+    fetchProfile();
+  }, [auth]);
 
   return (
-     
     <div className="flex flex-col md:flex-row items-start p-8">
-      <ProfileNavigation userInfo={UserInfo} />      
-      
+      <ProfileNavigation userInfo={userInfo} />      
       <div className="bg-white shadow-md rounded-lg overflow-hidden w-full max-w-4xl flex flex-col md:flex-row mt-8">
         <div className="flex-1 p-4 flex flex-col justify-between">
           <section>
@@ -17,45 +37,40 @@ const Profile = () => {
               <h3 className="text-lg font-bold mb-2">IDENTIFICATION DATA</h3>
             </div>
             <div className="mt-4">
-              
               <div className="grid grid-cols-2 gap-4">
                 <div>
                   <label className="block font-bold" htmlFor="firstName">
                     Name
                   </label>
-                  <span>{UserInfo.firstName}</span>
+                  <span>{userInfo.firstName}</span>
                 </div>
                 <div>
                   <label className="block font-bold" htmlFor="lastName">
                     Surname
                   </label>
-                  <span>{UserInfo.lastName}</span>
+                  <span>{userInfo.lastName}</span>
                 </div>
                 <div>
                   <label className="block font-bold" htmlFor="lastName">
                     Email
                   </label>
-                  <span>{UserInfo.email}</span>
+                  <span>{userInfo.email}</span>
                 </div>
                 <div>
                   <label className="block font-bold" htmlFor="phone">
                     Phone
                   </label>
-                  <span>{UserInfo.phone}</span>
+                  <span>{userInfo.phone}</span>
                 </div>
-
                 <div>
                   <label className="block font-bold" htmlFor="registrationDate">
                     Registration Date
                   </label>
-                  <span>{UserInfo.registrationDate}</span>
+                  <span>{new Date(userInfo.registrationDate).toLocaleDateString()}</span>
                 </div>
-                
               </div>
-              
             </div>
           </section>
-
         </div>
       </div>
     </div>
