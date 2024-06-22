6fa3e9580e0343258ec6d6a23939a63f5cc17a60
alejandramantillac <alejamantillac@gmail.com>
2024-06-22 15:39:10 -0500

    refactor: update manage profile page

--- front-end/src/Pages/MangeProfile/index.jsx
+++ front-end/src/Pages/MangeProfile/index.jsx
@@ -4,7 +4,7 @@ import { useAuth } from '../../Context/AuthContext';
 import { ApiConfig } from '../../config/ApiConfig';
 
 const ManageProfile = () => {
-  const [editing, setEditing] = useState(false);
+  const [editingField, setEditingField] = useState(null);
   const [userInfo, setUserInfo] = useState({
     firstName: '',
     lastName: '',
@@ -27,18 +27,18 @@ const ManageProfile = () => {
     fetchProfile();
   }, [auth]);
 
-  const saveChanges = async () => {
+  const saveChanges = async (field) => {
     try {
       const response = await auth.authFetch(ApiConfig.profile, {
         method: 'PUT',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ ...userInfo, password }),
+        body: JSON.stringify(field === 'password' ? { password } : { [field]: userInfo[field] }),
       });
 
       if (response.ok) {
-        setEditing(false);
+        setEditingField(null);
         setPassword('');
       } else {
         console.error("Failed to save changes", response.statusText);
@@ -52,64 +52,55 @@ const ManageProfile = () => {
     setUserInfo({ ...userInfo, [field]: value });
   };
 
-  const renderInputField = (label, field, type = "text") => (
-    <div>
-      <label className="block font-bold" htmlFor={field}>
-        {label}
-      </label>
-      {editing ? (
-        <input
-          type={type}
-          id={field}
-          className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 block w-full sm:text-sm border rounded-md p-1"
-          value={userInfo[field]}
-          onChange={(e) => handleInputChange(field, e.target.value)}
-        />
-      ) : (
-        <span>{userInfo[field]}</span>
+  const renderField = (label, field, type = "text", editable = true) => (
+    <div className="flex justify-between items-center border-b p-4">
+      <div className="flex-1">
+        <label className="block font-bold" htmlFor={field}>
+          {label}
+        </label>
+        {editingField === field ? (
+          <input
+            type={type}
+            id={field}
+            className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 block w-full sm:text-sm border rounded-md p-1"
+            value={field === 'password' ? password : userInfo[field]}
+            onChange={(e) => handleInputChange(field, e.target.value)}
+          />
+        ) : (
+          <span>{field === 'password' ? '********' : userInfo[field]}</span>
+        )}
+      </div>
+      {editable && (
+        <div>
+          {editingField === field ? (
+            <button
+              onClick={() => saveChanges(field)}
+              className="bg-black text-white py-2 px-4 rounded">
+              Save
+            </button>
+          ) : (
+            <button
+              onClick={() => setEditingField(field)}
+              className="bg-black text-white py-2 px-4 rounded">
+              Edit
+            </button>
+          )}
+        </div>
       )}
     </div>
   );
 
   return (
-    <div className="flex flex-col md:flex-row items-start p-8">
+    <div className="flex flex-row items-start p-8">
       <ProfileNavigation userInfo={userInfo} />
-      <div className="bg-white shadow-md rounded-lg overflow-hidden w-full max-w-4xl flex flex-col md:flex-row mt-8">
-        <div className="flex-1 p-4 flex flex-col justify-between">
-          <section>
-            <h2 className="text-xl font-bold mb-4">PERSONAL INFORMATION</h2>
-            <div className="mt-4 grid gap-4">
-              {renderInputField("Name", "firstName")}
-              {renderInputField("Surname", "lastName")}
-              {renderInputField("Email", "email", "email")}
-              {renderInputField("Phone", "phone", "tel")}
-            </div>
-          </section>
-          <section>
-            <label className="block font-bold mt-4">Password:</label>
-            {editing ? (
-              <input
-                type="password"
-                id="password"
-                className="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 block w-full sm:text-sm border rounded-md p-1"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            ) : (
-              <span>********</span>
-            )}
-          </section>
-          <div className="mt-4">
-            {editing ? (
-              <button onClick={saveChanges} className="bg-black hover:font-bold text-white py-2 px-4 rounded">
-                Save Changes
-              </button>
-            ) : (
-              <button onClick={() => setEditing(true)} className="bg-black hover:font-bold text-white py-2 px-4 rounded">
-                Edit Profile
-              </button>
-            )}
-          </div>
+      <div className="bg-white shadow-md rounded-lg overflow-hidden w-full max-w-4xl ml-8">
+        <h2 className="text-xl font-bold p-4 border-b">Manage My Account</h2>
+        <div>
+          {renderField("Name", "firstName", "text", false)}
+          {renderField("Surname", "lastName", "text", false)}
+          {renderField("Email", "email", "email")}
+          {renderField("Phone", "phone", "tel")}
+          {renderField("Password", "password", "password")}
         </div>
       </div>
     </div>
