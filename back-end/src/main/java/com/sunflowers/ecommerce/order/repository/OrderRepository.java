96e3a3bfd0ef50f8a4b8592c9223500680ec4bb1
SilemNabib <silemnabib@gmail.com>
2024-06-22 16:53:02 -0500

    feat: get user orders endpoint

--- back-end/src/main/java/com/sunflowers/ecommerce/order/repository/OrderRepository.java
+++ back-end/src/main/java/com/sunflowers/ecommerce/order/repository/OrderRepository.java
@@ -2,10 +2,13 @@ package com.sunflowers.ecommerce.order.repository;
 
 import com.sunflowers.ecommerce.auth.entity.User;
 import com.sunflowers.ecommerce.order.entity.Order;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
-import java.util.Optional;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
 import java.util.UUID;
 
-public interface OrderRepository  extends CrudRepository<Order, UUID> {
+public interface OrderRepository extends PagingAndSortingRepository<Order, UUID> {
+    Page<Order> findAllByUser(User user, Pageable pageable);
 }
