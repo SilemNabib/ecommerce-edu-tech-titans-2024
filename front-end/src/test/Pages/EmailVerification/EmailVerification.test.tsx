fc05e977ccffc407726916da95ee8fb293ccb9b4
NaySp <113195891+NaySp@users.noreply.github.com>
2024-06-22 14:54:48 -0500

    test: add more tests for email verification front-end

--- front-end/src/test/Pages/EmailVerification/EmailVerification.test.tsx
+++ front-end/src/test/Pages/EmailVerification/EmailVerification.test.tsx
@@ -1,15 +1,20 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
-import { beforeEach, describe, expect, it, vi} from "vitest";
+import { fireEvent, render, screen, waitFor} from "@testing-library/react";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { BrowserRouter as Router } from 'react-router-dom';
-// @ts-ignore
 import SendVerificationEmail from "../../../Pages/EmailVerification";
+
+
+vi.mock('../../../Context/AuthContext', () => ({
+  useAuth: vi.fn(() => ({
+    requestRegister: vi.fn(),
+  })),
+}));
 
 describe('EmailVerification', () => {
 
-
     beforeEach(() => {
         render(<Router>
-            <SendVerificationEmail  />
+            <SendVerificationEmail />
         </Router>)
     });
     
@@ -24,5 +29,45 @@ describe('EmailVerification', () => {
         expect(submitButton).toBeInTheDocument();
     });
     
+    it('validates email input', async () => {
+        const emailInput = screen.getByPlaceholderText('Enter your email');
+        const submitButton = screen.getByRole('button', { name: /send email/i });
+
+        // Intenta enviar el formulario vacío
+        fireEvent.click(submitButton);
+        await waitFor(() => {
+        expect(screen.getByText('This field is mandatory')).toBeInTheDocument();
+        });
+
+        // Ingresa un email inválido
+        fireEvent.change(emailInput, { target: { value: 'invalidemail' } });
+        fireEvent.click(submitButton);
+        await waitFor(() => {
+        expect(screen.getByText('This field is mandatory')).toBeInTheDocument();
+        });
+
+        // Ingresa un email válido
+        fireEvent.change(emailInput, { target: { value: 'valid@email.com' } });
+        fireEvent.click(submitButton);
+        await waitFor(() => {
+        expect(screen.queryByText('This field is mandatory')).not.toBeInTheDocument();
+        });
+    });
+
+    it('displays loading indicator when form is submitted', async () => {
+        const emailInput = screen.getByPlaceholderText('Enter your email');
+        const submitButton = screen.getByRole('button', { name: /send email/i });
+
+        fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+        fireEvent.click(submitButton);
+
+        await waitFor(() => {
+        expect(screen.getByRole('progressbar')).toBeInTheDocument();
+        });
+    });
+
+  
+ 
 
 });
+
