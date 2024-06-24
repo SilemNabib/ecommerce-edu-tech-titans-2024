c31c044a352eecb665e0e9d8e8b8f3d000df3067
alejandramantillac <alejamantillac@gmail.com>
2024-06-23 22:20:23 -0500

    feat: add orders list component on admin dashboard

--- front-end/src/Pages/AdminDashboard/index.jsx
+++ front-end/src/Pages/AdminDashboard/index.jsx
@@ -1,10 +1,25 @@
+import OrderType from '../../Components/OrderType';
 import Sidebar from "../../Components/Sidebar";
 
+const ordersData = {
+  newOrders: [{ date: '2023-04-01', status: 'NEW', total: 100 }],
+  preparedOrders: [{ date: '2023-04-02', status: 'PREPARED', total: 200 }],
+  sentOrders: [{ date: '2023-04-03', status: 'SENT', total: 300 }],
+  completedOrders: [{ date: '2023-04-04', status: 'COMPLETED', total: 400 }],
+};
+
 const AdminDashboard = () => {
-    return (
-        <div>
-            <Sidebar />
-        </div>
-    );
-}
+  return (
+    <div className="flex flex-col md:flex-row h-screen bg-gray-100">
+      <Sidebar />
+      <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 w-full">
+        <OrderType title="New Orders" orders={ordersData.newOrders} />
+        <OrderType title="Prepared Orders" orders={ordersData.preparedOrders} />
+        <OrderType title="Sent Orders" orders={ordersData.sentOrders} />
+        <OrderType title="Completed Orders" orders={ordersData.completedOrders} />
+      </div>
+    </div>
+  );
+};
+
 export default AdminDashboard;
