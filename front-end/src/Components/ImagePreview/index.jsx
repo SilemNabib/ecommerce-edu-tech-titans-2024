7cf5ea485a36ee73d5c5d32fc297dac054a4a8df
alejandramantillac <alejamantillac@gmail.com>
2024-06-25 00:18:33 -0500

    feat: add functionality upload product image

--- front-end/src/Components/ImagePreview/index.jsx
+++ front-end/src/Components/ImagePreview/index.jsx
@@ -4,7 +4,7 @@ const ImagePreview = ({ selectedImage }) => {
   return (
     <div className="w-3/4 h-full border border-gray-200 rounded-md bg-white shadow-md flex items-center justify-center p-4">
       {selectedImage ? (
-        <img src={selectedImage} alt="preview" className="max-w-full max-h-full" />
+        <img src={selectedImage.url} alt="preview" className="max-w-full max-h-full" />
       ) : (
         <span className="text-gray-500">Image Preview</span>
       )}
