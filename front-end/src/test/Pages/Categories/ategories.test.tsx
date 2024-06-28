3888e58e6416d49fd8e12a11bc43d830213622a5
NaySp <113195891+NaySp@users.noreply.github.com>
2024-06-28 00:19:21 -0500

    feature: change file name

--- front-end/src/test/Pages/Categories/ategories.test.tsx
+++ front-end/src/test/Pages/Categories/ategories.test.tsx
@@ -1,4 +1,3 @@
-import '@testing-library/jest-dom'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { describe, expect, it, vi } from "vitest"
